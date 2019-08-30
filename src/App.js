137582f188215effb0c1ccbcc8b574069bc18994
Seham1981 <seham.hammad@yahoo.ie>
2019-08-30 22:17:37 +0100

    weather-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//the wrapper component
+//state is an object that has key value pairs
+// never manipulate the state directly with this.state.value 
+// use the method this.setState to change ot update the state
+import React from "react";
+
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "d5299d3e2ffd430a0bc21e2991bf176d";
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the values."
+      });
+    }
+  }
+  render() {
+      return (
+        <div>
+          <div className="wrapper">
+            
+             
+                <div className="row">
+                  <div className="col-xs-5 title-container">
+                    <Titles />
+                  </div>
+                  <div className="col-xs-5 form-container">
+                    <Form getWeather={this.getWeather} />
+                    <Weather 
+                      temperature={this.state.temperature} 
+                      humidity={this.state.humidity}
+                      city={this.state.city}
+                      country={this.state.country}
+                      description={this.state.description}
+                      error={this.state.error}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          
+        
+      );
+  }
+};
 
 export default App;
+
+
+
+
+
