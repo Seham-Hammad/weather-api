85dccecd9f220b689b0e7bbd237b5b81981a3757
Seham1981 <seham.hammad@yahoo.ie>
2019-08-30 22:40:58 +0100

    weather-app

---
+++ src/components/Titles.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather Finder</h1>
+		<h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
+	</div>
+);
+
+export default Titles;
+
+/**
+ * class Title extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1> Weather Finder</h1>
+                <h3>Find out Temp, conditions and more ...</h3>
+            </div>
+        )
+    }
+
+}
+export default Title;
+ */
